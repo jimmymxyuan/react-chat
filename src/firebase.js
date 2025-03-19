726ee093b725dfb45b3f14cd6bc4f2570d6ed517
eljimo <jimmy.mx.yuan@gmail.com>
2025-03-20 00:12:07 +1100

    final check
    Firebase auth doesn't work... user is always null, will try a different service

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -13,9 +15,11 @@ const firebaseConfig = {
   storageBucket: "react-chat-bg.firebasestorage.app",
   messagingSenderId: "635626289312",
   appId: "1:635626289312:web:e7a529e7548fef335d71c5",
-  measurementId: "G-J9M3REH2WP"
+  //measurementId: "G-J9M3REH2WP"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
+export const auth = getAuth(app)
+export const db = getFirestore(app)
